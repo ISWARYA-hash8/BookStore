fc74dae71ea14b03d2f470d06e6f0b490faf0a17
ISWARYA835 <iswarya83p@gmail.com>
2025-05-29 19:26:33 +0530

    final commit

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import axios from 'axios'
+
+const Spinner = () => {
+  return (
+    <div className='animate-ping w-16 h-16 m-8 rounded-sm bg-sky-600'>Spinner</div>
+  )
+}
+
+export default Spinner

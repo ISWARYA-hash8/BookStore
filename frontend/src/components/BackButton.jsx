9a3d660a6517750e89adde66291b61f07e57a72f
ISWARYA835 <iswarya83p@gmail.com>
2025-06-05 23:08:31 +0530

    css change

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -3,7 +3,7 @@ import { BsArrowLeft } from "react-icons/bs";
 
 const BackButton = ({destination = '/'})=>{
     return (
-        <div className="flex m-4">
+        <div className="flex m-4 p-4">
             <Link to = {destination} className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit">
 <BsArrowLeft className=" text-2xl"/>
 
